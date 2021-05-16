85294987a0c4eec90976f446e672afadf9554149
David Ramirez <david.9706@hotmail.com>
2021-05-16 15:12:20 -0700

    Add about and home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'pages#home'#send it to application but we might want to define it as the action such asserving pages ..pages controller 
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
